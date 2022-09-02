575f4465c31bb74c8f7682e9cf3d45e6132c2e61
ricardo55991 <ricardo.barros.jr@gmail.com>
2022-09-02 01:34:44 -0300

    Ajustes no design

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,28 @@
 <template>
-  <div>
-    <h2>Login</h2>
-    <form>
-      <v-btn>texte</v-btn>
-      <div class="mb-3">
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Usuário">
+  <div class="container-fluid">
+    <div class="row d-flex justify-content-around">
+      <div class="col-4 mt-0">
+        <img class="totoro-img img-fluid" src="../assets/totoro.png" alt="totoro" />
       </div>
-      <div class="mb-3">
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+
+      <div class="col-5">
+        <section>
+          <h2>Entrar</h2>
+        </section>
+        <form>
+          <div class="mb-3">
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+              placeholder="Usuário">
+          </div>
+          <div class="mb-3">
+            <input type="password" class="form-control" style="border-top: none" id="exampleInputPassword1" placeholder="Senha">
+          </div>
+          <button type="submit" class="btn btn-primary">Entrar</button>
+        </form>
       </div>
-      <button type="submit" class="btn btn-primary">Entrar</button>
-    </form>
+    </div>
   </div>
-  
+
 </template>
 
 <script>
@@ -22,5 +32,17 @@ export default {
 </script>
 
 <style>
+body{
+  background-color: #012E40 !important;
+}
 
+.totoro-img{
+  max-height: 1080px;
+}
+
+.form-control{
+  background-color: #F2E3D5 !important;
+  padding-left: 23px !important;
+  height: 75px;
+}
 </style>
