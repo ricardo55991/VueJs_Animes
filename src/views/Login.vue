61b7b84bb9a3664cebe4fc092aa5b635e4153c89
ricardo55991 <ricardo.barros.jr@gmail.com>
2022-09-01 22:16:30 -0300

    Template Login

---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-btn>teste</v-btn>
+    <h2>Login</h2>
+    <form>
+      <div class="mb-3">
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Usuário">
+      </div>
+      <div class="mb-3">
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+      </div>
+      <button type="submit" class="btn btn-primary">Entrar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
